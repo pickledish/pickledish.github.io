3b75ec7b4bb86a9896fbca19cefa0f0b7f42923d
Brandon Willett <brandon@Brandons-MacBook-Pro.local>
2016-05-16 21:02:59 -0400

    Finishing

---
+++ main.js
@@ -0,0 +1,36 @@
+$(document).ready(function() {
+
+	$('.content').hide();
+	$('#c1').show();
+
+    $("#one").click(function() {
+        $('.content').hide();
+        $('#c1').show();
+    });
+
+    $("#two").click(function() {
+        $('.content').hide();
+        $('#c2').show();
+    });
+
+    $("#three").click(function() {
+        $('.content').hide();
+        $('#c3').show();
+    });
+
+    $("#four").click(function() {
+        $('.content').hide();
+        $('#c4').show();
+    });
+
+    $("#five").click(function() {
+        $('.content').hide();
+        $('#c5').show();
+    });
+
+    $("#six").click(function() {
+        $('.content').hide();
+        $('#c6').show();
+    });
+
+});
